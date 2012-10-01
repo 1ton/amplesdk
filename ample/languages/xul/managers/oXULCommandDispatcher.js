5e9c64654095eeabcb8211798d5051816778f02b
1ton <git@1ton.ru>
2012-10-01 21:13:32 +0400

    pull from upstream

--- ample/languages/xul/managers/oXULCommandDispatcher.js
+++ ample/languages/xul/managers/oXULCommandDispatcher.js
@@ -1,7 +1,7 @@
 /*
  * Ample SDK - JavaScript GUI Framework
  *
- * Copyright (c) 2009 Sergey Ilinsky
+ * Copyright (c) 2012 Sergey Ilinsky
  * Dual licensed under the MIT and GPL licenses.
  * See: http://www.amplesdk.com/about/licensing/
  *
@@ -17,11 +17,11 @@ cXULElement.prototype.doCommand		= function() {
 			oCommand	= null;
 	}
 
-    // Fire Event on command element
+	// Fire Event on command element
 	if (oCommand) {
-	    var oEvent = this.ownerDocument.createEvent("CustomEvent");
-	    oEvent.initCustomEvent("command", true, true, null);
-	    oCommand.dispatchEvent(oEvent);
+		var oEvent	= this.ownerDocument.createEvent("CustomEvent");
+		oEvent.initCustomEvent("command", true, true, null);
+		oCommand.dispatchEvent(oEvent);
 	}
 };
 
